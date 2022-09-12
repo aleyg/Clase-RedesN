d28895dd3f8ce0885910b507b6d850fb984752a5
aleyg <ale_yg@live.com>
2022-09-11 19:53:19 -0500

    Implementación de la función de la tangente hiperbólica

--- DeepLearningPython/network.py
+++ DeepLearningPython/network.py
@@ -41,7 +41,7 @@ class Network(object):
     def feedforward(self, a):
         """Return the output of the network if ``a`` is input."""
         for b, w in zip(self.biases, self.weights):
-            a = sigmoid(np.dot(w, a)+b)
+            a = tanh(np.dot(w, a)+b)
         return a
 
     def SGD(self, training_data, epochs, mini_batch_size, eta,
@@ -90,13 +90,14 @@ class Network(object):
         self.biases = [b-(eta/len(mini_batch))*nb
                        for b, nb in zip(self.biases, nabla_b)]
 
+
     def backprop(self, x, y):
         """Return a tuple ``(nabla_b, nabla_w)`` representing the
         gradient for the cost function C_x.  ``nabla_b`` and
         ``nabla_w`` are layer-by-layer lists of numpy arrays, similar
         to ``self.biases`` and ``self.weights``."""
-        nabla_b = [np.zeros(b.shape) for b in self.biases]
-        nabla_w = [np.zeros(w.shape) for w in self.weights]
+        nabla_b = [np.zeros(b.shape) for b in self.biases] # self.biases = [np.random.randn(y, 1) for y in sizes[1:]]
+        nabla_w = [np.zeros(w.shape) for w in self.weights] # self.weights = [np.random.randn(y, x) for x, y in zip(sizes[:-1], sizes[1:])]
         # feedforward
         activation = x
         activations = [x] # list to store all the activations, layer by layer
@@ -104,11 +105,11 @@ class Network(object):
         for b, w in zip(self.biases, self.weights):
             z = np.dot(w, activation)+b
             zs.append(z)
-            activation = sigmoid(z)
+            activation = tanh(z)
             activations.append(activation)
         # backward pass
         delta = self.cost_derivative(activations[-1], y) * \
-            sigmoid_prime(zs[-1])
+            tanh_prime(zs[-1])
         nabla_b[-1] = delta
         nabla_w[-1] = np.dot(delta, activations[-2].transpose())
         # Note that the variable l in the loop below is used a little
@@ -119,7 +120,7 @@ class Network(object):
         # that Python can use negative indices in lists.
         for l in range(2, self.num_layers):
             z = zs[-l]
-            sp = sigmoid_prime(z)
+            sp = tanh_prime(z)
             delta = np.dot(self.weights[-l+1].transpose(), delta) * sp
             nabla_b[-l] = delta
             nabla_w[-l] = np.dot(delta, activations[-l-1].transpose())
@@ -140,6 +141,14 @@ class Network(object):
         return (output_activations-y)
 
 #### Miscellaneous functions
+def tanh(z):
+    "Tangente hiperbólica"
+    return (2.0/(1.0+np.exp(-z)))+1
+
+def tanh_prime(z):
+    "Derivada de la tangente hiperbólica"
+    return 4*(np.exp(-2*z))/(np.exp(-2*z)+1)**2
+
 def sigmoid(z):
     """The sigmoid function."""
     return 1.0/(1.0+np.exp(-z))
--- DeepLearningPython/prueba.py
+++ DeepLearningPython/prueba.py
@@ -9,7 +9,7 @@ DTraining = list(DTraining)
 DTest = list(DTest)
 
 net = network.Network([784, 30, 10])
-net.SGD(DTraining, 30, 10, 3.0, test_data = DTest)
+net.SGD(DTraining, 30, 10, 0.5, test_data = DTest)
 
 File = open('red_prueba.pkl','wb')
 pickle.dump(net, File)
